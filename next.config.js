424dd0e4a7b6746ef6ec5ca6b6dce1b0acbff88b
mc30078 <60971463+mc30078@users.noreply.github.com>
2026-05-28 16:38:47 -0400

    Clear nextConfig in next.config.js
    Removed all configuration options from nextConfig.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compress: true,
-  poweredByHeader: false,
-  generateEtags: true,
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  experimental: {
-    // Add experimental features as needed
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
